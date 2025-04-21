072473c6293be9c9df53e3be2ad8e3ebfcb55f69
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-04-21 06:14:03 +0545

    hashing

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,10 +35,23 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $password = $request->password;
+        $cost = 10; // Cost factor for bcrypt (can be adjusted)
+
+        // Create a random salt
+        $salt = substr(base64_encode(openssl_random_pseudo_bytes(17)), 0, 22);
+        $salt = strtr($salt, '+', '.'); // bcrypt uses ./ instead of + in the salt
+
+        // Build the bcrypt salt format
+        $salt = sprintf('$2y$%02d$', $cost) . $salt;
+
+        // Hash the password with bcrypt
+        $hashedPassword = crypt($password, $salt);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $hashedPassword,
         ]);
 
         event(new Registered($user));
